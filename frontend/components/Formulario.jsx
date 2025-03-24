f36714497f2b6961387ece5a2c228ba9119e7e85
rafatreggia <rafaeltreggia24@gmail.com>
2025-03-24 08:33:29 -0300

    Hero con descarga de CV

--- frontend/components/Formulario.jsx
+++ frontend/components/Formulario.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div id='formulario'>Formulario</div>
   )
 }
 
--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { BoxReveal } from "./magicui/box-reveal";
+import { Download, Mail } from "lucide-react";
 
 const Hero = () => {
   return (
@@ -24,8 +25,10 @@ const Hero = () => {
         </BoxReveal>
         <BoxReveal boxColor={"#58AF9B"} duration={1}>
           <div className="mt-6 space-x-5">
-            <Button>click here</Button>
-            <Button>click here</Button>
+            <Button><Mail />Contacto</Button>
+            <a  href="/documentos/cvRafa.pdf" download className="border-gray-900 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground
+            inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-b-[2px] border-r-[2px] h-9 px-4 py-2 cursor-pointer"><Download />Descargar CV</a>
+            
           </div>
         </BoxReveal>
       </div>
