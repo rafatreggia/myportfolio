22629df46987e978d68bab2a9d2de8e1982a20ac
rafatreggia <rafaeltreggia24@gmail.com>
2025-03-28 10:15:57 -0300

    trabaje sobre footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,27 +1,46 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
     <div
       id="footer"
-      className="pb-[150px] py-5 px-5 mt-20 w-full bg-[#58AF9B] "
+      className="pb-[150px] py-5 px-5 mt-20 w-full bg-slate-900  "
     >
-      <div className="flex items-center justify-between pr-20 pl-10 w-full">
+      <div className="flex lg:flex-row items-center justify-between pr-20 pl-10 w-full flex-col gap-5">
         <div className="flex items-center gap-5 ">
-          <img
-            src="/images/imgrafafooter.webp"
-            alt="imagen footer"
-            className="w-30 h-32 rounded-full bg-white  "
-          />
-          <h1 className="text-[60px] font-semibold ">Treggia Rafael</h1>
+          <div className="sm:w-[100px] sm:h-[100px] rounded-full overflow-hidden w-[50px] h-[50px] ">
+            <img
+              src="/images/imgrafafooter.webp"
+              alt="imagen footer"
+              className="w-full h-full object-cover  bg-white  "
+            />
+          </div>
+          <div className="hidden lg:flex">
+            <div className="lg:w-[500px] overflow-hidden h-[45px] lg:h-[90px]  ">
+              <motion.div whileHover={{ y: "-100%" }} className="relative">
+                <h1 className="text-white text-[32px] lg:text-[60px] ml-6 lg:ml-12">
+                  Contacto
+                </h1>
+                <h1 className="text-white text-[32px] lg:text-[60px]  ml-12 absolute">
+                  Treggia Rafael
+                </h1>
+              </motion.div>
+                        
+            </div>
+          </div>
+
+          <h1 className="flex lg:hidden text-white text-[32px]">
+            Treggia Rafael
+          </h1>
         </div>
 
-        <h2 className="text-end font-semibold text-[18px] max-w-[300px]">
-          Si te intereso mi perfil, no dudes en ponerte en contacto!
+        <h2 className="lg:text-end text-center font-semibold text-[20px] text-nowrap text-white">
+          Si te intereso mi perfil,<br></br> no dudes en ponerte en contacto!
         </h2>
       </div>
-      <div className="pl-10 flex items-center justify-start mt-20 gap-3">
+      <div className="pl-10 flex items-center justify-center lg:justify-start  mt-20 gap-3 ">
         <a
           href="https://wa.me/5493512413771"
           target="_blank"
@@ -46,19 +65,62 @@ const Footer = () => {
           </svg>
         </a>
         <a
-          href="https://www.instagram.com/tu_usuario"
+          href="https://www.instagram.com/rafatreggia"
           target="_blank"
           rel="noopener noreferrer"
         >
           <svg
             className="size-8"
-            viewBox="0 0 24 24"
-            width="256"
-            height="259"
             xmlns="http://www.w3.org/2000/svg"
-            fill="#E1306C"
+            width="256"
+            height="256"
+            viewBox="0 0 256 256"
           >
-            <path d="M12 2.2c3.2 0 3.6 0 4.8.1 1.2.1 1.8.3 2.3.6.5.2.9.6 1.3 1 .4.4.8.8 1 1.3.3.5.5 1.1.6 2.3.1 1.2.1 1.6.1 4.8s0 3.6-.1 4.8c-.1 1.2-.3 1.8-.6 2.3-.2.5-.6.9-1 1.3-.4.4-.8.8-1.3 1-.5.3-1.1.5-2.3.6-1.2.1-1.6.1-4.8.1s-3.6 0-4.8-.1c-1.2-.1-1.8-.3-2.3-.6a4 4 0 0 1-1.3-1 4 4 0 0 1-1-1.3c-.3-.5-.5-1.1-.6-2.3C2.2 15.6 2.2 15.2 2.2 12s0-3.6.1-4.8c.1-1.2.3-1.8.6-2.3a4 4 0 0 1 1-1.3 4 4 0 0 1 1.3-1c.5-.3 1.1-.5 2.3-.6 1.2-.1 1.6-.1 4.8-.1zM12 0C8.7 0 8.3 0 7 .1 5.7.1 4.7.3 3.9.7a6 6 0 0 0-2.2 1.4 6 6 0 0 0-1.4 2.2C0 6.3 0 7.3 0 9.6V12c0 2.4 0 3.3.1 4.4.1 1.3.3 2.3.7 3.1a6 6 0 0 0 1.4 2.2 6 6 0 0 0 2.2 1.4c.8.3 1.8.6 3.1.7 1.1.1 2 .1 4.4.1s3.3 0 4.4-.1c1.3-.1 2.3-.3 3.1-.7a6 6 0 0 0 2.2-1.4 6 6 0 0 0 1.4-2.2c.3-.8.6-1.8.7-3.1.1-1.1.1-2 .1-4.4s0-3.3-.1-4.4c-.1-1.3-.3-2.3-.7-3.1a6 6 0 0 0-1.4-2.2 6 6 0 0 0-2.2-1.4c-.8-.3-1.8-.6-3.1-.7C15.3 0 14.3 0 12 0zM12 5.8a6.2 6.2 0 1 0 0 12.4 6.2 6.2 0 0 0 0-12.4zm0 10.2a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.4-10.9a1.4 1.4 0 1 1-2.8 0 1.4 1.4 0 0 1 2.8 0z" />
+            <g fill="none">
+              <rect
+                width="256"
+                height="256"
+                fill="url(#skillIconsInstagram0)"
+                rx="60"
+              />
+              <rect
+                width="256"
+                height="256"
+                fill="url(#skillIconsInstagram1)"
+                rx="60"
+              />
+              <path
+                fill="#fff"
+                d="M128.009 28c-27.158 0-30.567.119-41.233.604c-10.646.488-17.913 2.173-24.271 4.646c-6.578 2.554-12.157 5.971-17.715 11.531c-5.563 5.559-8.98 11.138-11.542 17.713c-2.48 6.36-4.167 13.63-4.646 24.271c-.477 10.667-.602 14.077-.602 41.236s.12 30.557.604 41.223c.49 10.646 2.175 17.913 4.646 24.271c2.556 6.578 5.973 12.157 11.533 17.715c5.557 5.563 11.136 8.988 17.709 11.542c6.363 2.473 13.631 4.158 24.275 4.646c10.667.485 14.073.604 41.23.604c27.161 0 30.559-.119 41.225-.604c10.646-.488 17.921-2.173 24.284-4.646c6.575-2.554 12.146-5.979 17.702-11.542c5.563-5.558 8.979-11.137 11.542-17.712c2.458-6.361 4.146-13.63 4.646-24.272c.479-10.666.604-14.066.604-41.225s-.125-30.567-.604-41.234c-.5-10.646-2.188-17.912-4.646-24.27c-2.563-6.578-5.979-12.157-11.542-17.716c-5.562-5.562-11.125-8.979-17.708-11.53c-6.375-2.474-13.646-4.16-24.292-4.647c-10.667-.485-14.063-.604-41.23-.604zm-8.971 18.021c2.663-.004 5.634 0 8.971 0c26.701 0 29.865.096 40.409.575c9.75.446 15.042 2.075 18.567 3.444c4.667 1.812 7.994 3.979 11.492 7.48c3.5 3.5 5.666 6.833 7.483 11.5c1.369 3.52 3 8.812 3.444 18.562c.479 10.542.583 13.708.583 40.396s-.104 29.855-.583 40.396c-.446 9.75-2.075 15.042-3.444 18.563c-1.812 4.667-3.983 7.99-7.483 11.488c-3.5 3.5-6.823 5.666-11.492 7.479c-3.521 1.375-8.817 3-18.567 3.446c-10.542.479-13.708.583-40.409.583c-26.702 0-29.867-.104-40.408-.583c-9.75-.45-15.042-2.079-18.57-3.448c-4.666-1.813-8-3.979-11.5-7.479s-5.666-6.825-7.483-11.494c-1.369-3.521-3-8.813-3.444-18.563c-.479-10.542-.575-13.708-.575-40.413s.096-29.854.575-40.396c.446-9.75 2.075-15.042 3.444-18.567c1.813-4.667 3.983-8 7.484-11.5s6.833-5.667 11.5-7.483c3.525-1.375 8.819-3 18.569-3.448c9.225-.417 12.8-.542 31.437-.563zm62.351 16.604c-6.625 0-12 5.37-12 11.996c0 6.625 5.375 12 12 12s12-5.375 12-12s-5.375-12-12-12zm-53.38 14.021c-28.36 0-51.354 22.994-51.354 51.355s22.994 51.344 51.354 51.344c28.361 0 51.347-22.983 51.347-51.344c0-28.36-22.988-51.355-51.349-51.355zm0 18.021c18.409 0 33.334 14.923 33.334 33.334c0 18.409-14.925 33.334-33.334 33.334s-33.333-14.925-33.333-33.334c0-18.411 14.923-33.334 33.333-33.334"
+              />
+              <defs>
+                <radialGradient
+                  id="skillIconsInstagram0"
+                  cx="0"
+                  cy="0"
+                  r="1"
+                  gradientTransform="matrix(0 -253.715 235.975 0 68 275.717)"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#fd5" />
+                  <stop offset=".1" stop-color="#fd5" />
+                  <stop offset=".5" stop-color="#ff543e" />
+                  <stop offset="1" stop-color="#c837ab" />
+                </radialGradient>
+                <radialGradient
+                  id="skillIconsInstagram1"
+                  cx="0"
+                  cy="0"
+                  r="1"
+                  gradientTransform="matrix(22.25952 111.2061 -458.39518 91.75449 -42.881 18.441)"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#3771c8" />
+                  <stop offset=".128" stop-color="#3771c8" />
+                  <stop offset="1" stop-color="#60f" stop-opacity="0" />
+                </radialGradient>
+              </defs>
+            </g>
           </svg>
         </a>
         <a
@@ -67,14 +129,47 @@ const Footer = () => {
           rel="noopener noreferrer"
         >
           <svg
-            className="size-8"
-            viewBox="0 0 24 24"
-            width="256"
-            height="259"
+            className="size-9"
             xmlns="http://www.w3.org/2000/svg"
-            fill="#1877F2"
+            width="128"
+            height="128"
+            viewBox="0 0 128 128"
           >
-            <path d="M22.675 0H1.325C.593 0 0 .593 0 1.326v21.348C0 23.407.593 24 1.325 24h11.495v-9.294H9.428V11.29h3.392V8.413c0-3.36 2.05-5.197 5.048-5.197 1.436 0 2.669.107 3.027.154v3.51h-2.078c-1.627 0-1.943.774-1.943 1.91v2.5h3.885l-.507 3.416h-3.378V24h6.627c.732 0 1.325-.593 1.325-1.326V1.326C24 .593 23.407 0 22.675 0z" />
+            <rect
+              width="118.35"
+              height="118.35"
+              x="4.83"
+              y="4.83"
+              fill="#3d5a98"
+              rx="6.53"
+              ry="6.53"
+            />
+            <path
+              fill="#fff"
+              d="M86.48 123.17V77.34h15.38l2.3-17.86H86.48v-11.4c0-5.17 1.44-8.7 8.85-8.7h9.46v-16A127 127 0 0 0 91 22.7c-13.62 0-23 8.3-23 23.61v13.17H52.62v17.86H68v45.83z"
+            />
+          </svg>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/rafael-treggia-189a631b4/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            className="size-9"
+            xmlns="http://www.w3.org/2000/svg"
+            width="256"
+            height="256"
+            viewBox="0 0 256 256"
+          >
+            <g fill="none">
+              <rect width="256" height="256" fill="#fff" rx="60" />
+              <rect width="256" height="256" fill="#0a66c2" rx="60" />
+              <path
+                fill="#fff"
+                d="M184.715 217.685h29.27a4 4 0 0 0 4-3.999l.015-61.842c0-32.323-6.965-57.168-44.738-57.168c-14.359-.534-27.9 6.868-35.207 19.228a.32.32 0 0 1-.595-.161V101.66a4 4 0 0 0-4-4h-27.777a4 4 0 0 0-4 4v112.02a4 4 0 0 0 4 4h29.268a4 4 0 0 0 4-4v-55.373c0-15.657 2.97-30.82 22.381-30.82c19.135 0 19.383 17.916 19.383 31.834v54.364a4 4 0 0 0 4 4M38 59.628c0 11.864 9.767 21.626 21.632 21.626c11.862-.001 21.623-9.769 21.623-21.631C81.253 47.761 71.491 38 59.628 38C47.762 38 38 47.763 38 59.627m6.959 158.058h29.307a4 4 0 0 0 4-4V101.66a4 4 0 0 0-4-4H44.959a4 4 0 0 0-4 4v112.025a4 4 0 0 0 4 4"
+              />
+            </g>
           </svg>
         </a>
       </div>
