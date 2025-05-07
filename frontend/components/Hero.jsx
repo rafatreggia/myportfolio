6e3b565fc15cd1450a3cea03b2e8fb137d8b623d
rafatreggia <rafaeltreggia24@gmail.com>
2025-05-06 23:44:01 -0300

    Añado nuevo CV

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
               Contacto
             </a>
             <a
-              href="/documentos/cvRafa.pdf"
+              href="/documentos/cvRafaUltimo.pdf"
               download
               className="border-gray-900 border bg-background shadow-sm hover:bg-accent hover:text-accent-foreground
             inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-b-[2px] border-r-[2px] h-9 px-4 py-2 cursor-pointer"
